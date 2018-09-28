16c5b780ded9a27b56bf8570b3383768b08138f0
Student <Student@L411-S03>
2018-09-28 11:24:55 +0800

    added codes

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,23 @@
+
+package javaapplication2;
+
+
+public class JavaApplication2 {
+
+   
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+        
+        System.out.println("Welcome Github !!");
+        
+        
+        
+        
+        
+    }
+    
+}
