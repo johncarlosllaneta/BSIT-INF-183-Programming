e1842f5b827b709fd92852612453c1961d778e54
Student <Student@L411-S03>
2018-09-28 12:33:10 +0800

    added codes

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -1,22 +1,41 @@
 
 package javaapplication2;
 
+import java.util.Scanner;
+
 
 public class JavaApplication2 {
-
-   
+     /**
+     * 
+     * @param x
+     * @return 
+     */
+ public static double method(double base , int exponent ){
+     
+     int ans=1;
+        for(int c=1;c<=exponent;c++){
+            
+            ans *=base;
+        
+        }
+        System.out.println(base+" "+"^ "+exponent+" = "+ ans);
+                
+        return ans;
+    }
+ 
     public static void main(String[] args) {
+       Scanner s= new Scanner(System.in);
+        double x=0;
+       int y=0;
+       while(true){
+        System.out.println("Input Number : ");
+        x=s.nextDouble();
         
+        System.out.println("Input Number : ");
+        y=s.nextInt();
         
-        
-        
-        
-        
-        System.out.println("Welcome Github !!");
-        
-        
-        
-        
+                       method(x,y);
+       }    
         
     }
     
